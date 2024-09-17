0d7c82c978f2b2db6e0f0a00d38a209812c70f70
Alfonso Fortunato <alfofed@gmail.com>
2024-09-17 19:10:16 +0200

    feat(dotfile): slimmed down dotfile for working on ssh devpod containers

---
+++ dot_config/nixpkgs/config.nix
@@ -0,0 +1,47 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "alfonsofortunato-tools";
+      paths = [
+        bash-completion
+        neovim
+        git
+        htop
+        direnv
+        jetbrains-mono
+        (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+        curl
+        gnupg
+        neofetch
+        sops
+        fluxcd
+        wget
+        unzip
+        bat
+        lua
+        rustup
+        groovy
+        jq
+        yq
+        zsh
+        tmux
+        gnumake
+        python3
+        tmuxinator
+        wezterm
+        docker
+        go
+        nodejs_22
+        tmuxifier
+        fd
+        ripgrep
+        fzf
+        lazygit
+        kubectl
+        kubectx
+        yarn
+      ];
+    };
+  };
+}
+
